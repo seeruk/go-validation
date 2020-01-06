40d530f45983fadaf368fd393c9a12ba7a75fedd
Elliot Wright <wright.elliot@gmail.com>
2020-01-06 23:28:51 +0000

    Fixed Required declaration.

--- constraints/required.go
+++ constraints/required.go
@@ -3,6 +3,6 @@ package constraints
 import "github.com/seeruk/go-validation"
 
 // Required ...
-var Required validation.Constraint = func() []validation.ConstraintViolation {
+var Required validation.ConstraintFunc = func() []validation.ConstraintViolation {
 	return nil
 }
