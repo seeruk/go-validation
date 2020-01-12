b7e41943361e23b0384f0cd469af27ccc31d9274
Elliot Wright <wright.elliot@gmail.com>
2020-01-12 19:35:11 +0000

    Updated example struct tags.

--- example/example.go
+++ example/example.go
@@ -17,21 +17,21 @@ var timeYosemite = time.Date(1890, time.October, 1, 0, 0, 0, 0, time.UTC)
 // Example ...
 type Example struct {
 	Bool      bool                       `json:"bool,omitempty"`
-	Chan      <-chan string              `json:"chan" valley:"chan"`
+	Chan      <-chan string              `json:"chan" validation:"chan"`
 	Text      string                     `json:"text"`
-	Texts     []string                   `json:"texts" valley:"texts"`
+	Texts     []string                   `json:"texts" validation:"texts"`
 	TextMap   map[string]string          `json:"text_map"`
 	Adults    int                        `json:"adults"`
-	Children  int                        `json:"children" valley:"children"`
+	Children  int                        `json:"children" validation:"children"`
 	Int       int                        `json:"int"`
-	Int2      *int                       `json:"int2" valley:"int2"`
+	Int2      *int                       `json:"int2" validation:"int2"`
 	Ints      []int                      `json:"ints"`
-	Float     float64                    `json:"float" valley:"float"`
-	Time      time.Time                  `json:"time" valley:"time"`
+	Float     float64                    `json:"float" validation:"float"`
+	Time      time.Time                  `json:"time" validation:"time"`
 	Times     []time.Time                `json:"times"`
-	Nested    *NestedExample             `json:"nested" valley:"nested"`
+	Nested    *NestedExample             `json:"nested" validation:"nested"`
 	Nesteds   []*NestedExample           `json:"nesteds"`
-	NestedMap map[NestedExample]struct{} `json:"nested_map" valley:"nested_map"`
+	NestedMap map[NestedExample]struct{} `json:"nested_map" validation:"nested_map"`
 }
 
 func exampleConstraints() validation.Constraint {
