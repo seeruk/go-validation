f8ca2bd8625c2522b82166dfd54ae7ddc177c5d8
Elliot Wright <wright.elliot@gmail.com>
2020-01-13 15:08:18 +0000

    Added 'Min' constraint.

--- constraints/min.go
+++ constraints/min.go
@@ -6,8 +6,8 @@ import (
 	"github.com/seeruk/go-validation"
 )
 
-// Max ...
-func Max(max float64) validation.ConstraintFunc {
+// Min ...
+func Min(min float64) validation.ConstraintFunc {
 	return func(ctx validation.Context) []validation.ConstraintViolation {
 		rval := validation.UnwrapValue(ctx.Value().Node)
 		if validation.IsNillable(rval) && rval.IsNil() {
@@ -24,17 +24,17 @@ func Max(max float64) validation.ConstraintFunc {
 
 		switch rval.Kind() {
 		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-			violated = float64(rval.Int()) > max
+			violated = float64(rval.Int()) < min
 		case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
-			violated = float64(rval.Uint()) > max
+			violated = float64(rval.Uint()) < min
 		case reflect.Float32, reflect.Float64:
-			violated = rval.Float() > max
+			violated = rval.Float() < min
 		}
 
 		if violated {
 			return []validation.ConstraintViolation{
-				ctx.Violation("maximum value exceeded", map[string]interface{}{
-					"maximum": max,
+				ctx.Violation("minimum value not met", map[string]interface{}{
+					"minimum": min,
 				}),
 			}
 		}
---
+++ constraints/min_test.go
@@ -0,0 +1,61 @@
+package constraints
+
+import (
+	"math"
+	"testing"
+
+	"github.com/seeruk/go-validation"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestMin(t *testing.T) {
+	t.Run("should return no violations for a valid value", func(t *testing.T) {
+		assert.Empty(t, Min(0)(validation.NewContext(123)))
+		assert.Empty(t, Min(0)(validation.NewContext(uint(123))))
+		assert.Empty(t, Min(0)(validation.NewContext(123.456)))
+	})
+
+	t.Run("should return a violation if the maximum value is exceeded", func(t *testing.T) {
+		assert.NotEmpty(t, Min(math.MaxFloat64)(validation.NewContext(123)))
+		assert.NotEmpty(t, Min(math.MaxFloat64)(validation.NewContext(uint(123))))
+		assert.NotEmpty(t, Min(math.MaxFloat64)(validation.NewContext(123.456)))
+	})
+
+	t.Run("should return details about the maximum value with a violation", func(t *testing.T) {
+		violations := Min(math.MaxFloat64)(validation.NewContext(123))
+		require.Len(t, violations, 1)
+		assert.Equal(t, map[string]interface{}{
+			"minimum": math.MaxFloat64,
+		}, violations[0].Details)
+	})
+
+	t.Run("should not panic if given values of any regular numeric type", func(t *testing.T) {
+		assert.NotPanics(t, func() {
+			Min(0)(validation.NewContext(123))
+			Min(0)(validation.NewContext(int8(123)))
+			Min(0)(validation.NewContext(int16(123)))
+			Min(0)(validation.NewContext(int32(123)))
+			Min(0)(validation.NewContext(int64(123)))
+			Min(0)(validation.NewContext(uint(123)))
+			Min(0)(validation.NewContext(uint8(123)))
+			Min(0)(validation.NewContext(uint16(123)))
+			Min(0)(validation.NewContext(uint32(123)))
+			Min(0)(validation.NewContext(uint64(123)))
+			Min(0)(validation.NewContext(float32(123.456)))
+			Min(0)(validation.NewContext(123.456))
+		})
+	})
+
+	t.Run("should not panic if given a nil pointer to a numeric type", func(t *testing.T) {
+		assert.NotPanics(t, func() {
+			Min(1)(validation.NewContext((*int)(nil)))
+		})
+	})
+
+	t.Run("should panic if given a value of the wrong type", func(t *testing.T) {
+		assert.Panics(t, func() {
+			Min(1)(validation.NewContext("test"))
+		})
+	})
+}
